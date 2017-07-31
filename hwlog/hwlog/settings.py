d191c6d7a187822850eb1ff89b594f12b10f37a1
Danny Yu <whale1126@hotmail.com>
2017-07-31 23:48:54 +0800

    home page
    logo still needs fixing lol

--- hwlog/hwlog/settings.py
+++ hwlog/hwlog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'log.apps.LogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hwlog/log/views.py
+++ hwlog/log/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.views import generic
+
+
+def home(request):
+    return render(request,'log/index.html')
