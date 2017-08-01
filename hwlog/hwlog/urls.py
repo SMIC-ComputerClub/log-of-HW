05f3fd9c0c34ffb1a5ad4a3f9aef5aa9efeb9537
Danny Yu <whale1126@hotmail.com>
2017-08-01 21:17:20 +0800

    stuff

--- hwlog/hwlog/urls.py
+++ hwlog/hwlog/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from log import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.login, name = 'login'),
+    url(r'^logout/$', auth_views.logout, name = 'logout'),
     url(r'^log/', include('log.urls')),
     url(r'^admin/', admin.site.urls),
 ]
