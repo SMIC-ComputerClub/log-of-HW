3f6003d3a4dfcc763f80d3b3434d3a0429305a14
Danny Yu <whale1126@hotmail.com>
2017-08-02 22:03:33 +0800

    added configuration

--- hwlog/log/models.py
+++ hwlog/log/models.py
@@ -3,9 +3,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
 
 class Course(models.Model):
     course_name = models.CharField(max_length=50)
