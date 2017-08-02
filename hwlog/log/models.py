3080db83411fdac428f4f47a72ae19104f7d6958
Danny Yu <whale1126@hotmail.com>
2017-08-02 21:03:43 +0800

    added course and period models

--- hwlog/log/models.py
+++ hwlog/log/models.py
@@ -6,3 +6,18 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    code = models.CharField(max_length=9, default="*")
+    def __str__(self):
+        return self.course_name
+    def get_course_code(self):
+        return code
+
+class Period(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    time = models.IntegerField()
+    hw = models.TextField(max_length=500, blank=True)
+    def __int__(self):
+        return self.time
