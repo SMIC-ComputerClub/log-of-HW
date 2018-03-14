c3572c8fc7e91134fc05a64e0742bc50ce190881
Danny Yu <whale1126@hotmail.com>
2018-03-14 17:03:07 +0800

    previous homeworks changed to within a week

--- hwlog/log/views.py
+++ hwlog/log/views.py
@@ -12,6 +12,7 @@ from .models import Course
 from .forms import ClassEnrollForm, ChangeHWForm
 from .models import Homework
 from django.utils import timezone
+from datetime import datetime, timedelta
 
 def about(request):
     return render(request,'about.html')
@@ -53,55 +54,13 @@ def configure(request): #settings page, might be lots of bugs
             for x in range(0,7): #adding students to courses
                 coursearray.append(form.cleaned_data['course_'+str(x+1)])
                 coursearray[x].students.add(request.user)
-            #course_1 = form.cleaned_data['course_1']
-            #course_1.students.add(request.user)
-            #course_2 = form.cleaned_data['course_2']
-            #course_2.students.add(request.user)
-            #course_3 = form.cleaned_data['course_3']
-            #course_3.students.add(request.user)
-            #course_4 = form.cleaned_data['course_4']
-            #course_4.students.add(request.user)
-            #course_5 = form.cleaned_data['course_5']
-            #course_5.students.add(request.user)
-            #course_6 = form.cleaned_data['course_6']
-            #course_6.students.add(request.user)
-            #course_7 = form.cleaned_data['course_7']
-            #course_7.students.add(request.user)
             return redirect('home')
     else:
         user_courses = request.user.courses_joined.all()
         user_class = [ ]
         no_course = Course.objects.get(course_name='--None--')
 
-    #    try:                                                        #extremely inefficient i think
-    #        user_class_1 = user_courses[0]
-    #    except IndexError:
-    #        user_class_1 = no_course
-    #    try:
-    #        user_class_2 = user_courses[1]
-    #    except IndexError:
-    #        user_class_2 = no_course
-    #    try:
-    #        user_class_3 = user_courses[2]
-    #    except IndexError:
-    #        user_class_3 = no_course
-    #    try:
-    #        user_class_4 =user_courses[3]
-    #    except IndexError:
-    #        user_class_4 = no_course
-    #    try:
-    #        user_class_5 = user_courses[4]
-    #    except IndexError:
-    #        user_class_5 = no_course
-    #    try:
-    #        user_class_6 = user_courses[5]
-    #    except IndexError:
-    #        user_class_6 = no_course
-    #    try:
-    #        user_class_7 = user_courses[6]
-    #    except IndexError:
-    #        user_class_7 = no_course
-        for x in range(0,7):                    #loop to do the same thing as that^
+        for x in range(0,7): #create an array with user courses^
             try:
                 user_class.append(user_courses[x])
             except IndexError:
@@ -126,6 +85,8 @@ def detail(request, course_id): #page to edit hw
             return redirect('home')
     else:
         form = ChangeHWForm({'hw':course.get_latest_hw})
+
+    one_week_ago = timezone.now()-timedelta(days=7) #create date object to filter homework posted within a week
     return render(request, 'detail.html', {'form': form,
                                             'course': course,
-                                            'latest_hw_list': course.homework_set.order_by('-pub_date')[:7]})
+                                            'latest_hw_list': course.homework_set.filter(pub_date__gte=one_week_ago)})
